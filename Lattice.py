df6a516c235bcf7ca6f7d66478bf74d58ed810c3
Matteo <aguiarimatteo@gmail.com>
2025-07-21 15:16:12 +0200

    Added function to compute the active border of the crystal

--- Lattice.py
+++ Lattice.py
@@ -1,41 +1,54 @@
 import numpy as np
 
 class Lattice:
+    """
+    Attributes:
+    """
     def __init__(self, number_of_cells_x: int, number_of_cells_y: int, number_of_cells_z: int):
-        """_summary_
-
+        """
         Args:
-            number_of_cells_x (int): _description_
-            number_of_cells_y (int): _description_
-            number_of_cells_z (int): _description_
+            number_of_cells_x (int): number of cells in the lattice along the x direction
+            number_of_cells_y (int): number of cells in the lattice along the y direction
+            number_of_cells_z (int): number of cells in the lattice along the z direction
         """
         if number_of_cells_x < 0 or number_of_cells_y < 0 or number_of_cells_z < 0:
             raise ValueError('ERROR: the size of the lattice must be an integer bigger or equal to zero!')
             
         self.shape = (number_of_cells_x, number_of_cells_y, number_of_cells_z)
         self.grid = np.zeros(self.shape, dtype=np.uint8)    # 0 = empty; 1 = occupied
+        self.initial_seeds = []
 
     def __str__(self):
-        return f"Lattice has shape: {self.shape}\nNumber of occupied sites: {np.sum(self.grid)}"
+        return f"Lattice has shape: {self.shape} \
+            \nNucleation seeds: {self.initial_seeds} \
+            \nNumber of occupied sites: {np.sum(self.grid)}"
 
-    def is_point_inside(self, x: int, y: int, z: int):
-        """_summary_
+    def is_point_inside(self, x: int, y: int, z: int) -> bool:
+        """
+        Returns if a point (x,y,z) is or not inside the lattice.
 
         Args:
-            x (int): _description_
-            y (int): _description_
-            z (int): _description_
+            x (int): x coordinate of the point to verify
+            y (int): y coordinate of the point to verify
+            z (int): z coordinate of the point to verify
+            
+        Return:
+            (bool): returns if the point is inside the lattice or not
         """
         nx, ny, nz = self.shape
         return (0 <= x < nx) and (0 <= y < ny) and (0 <= z < nz)
         
-    def get_neighbors(self, x: int, y: int, z: int):
-        """_summary_
-
+    def get_neighbors(self, x: int, y: int, z: int) -> np.array:
+        """
+        Function that checks the occupation of the eight cells around the selected one.
+        
         Args:
-            x (int): _description_
-            y (int): _description_
-            z (int): _description_
+            x (int): x coordinate of the point to check the neighbors
+            y (int): y coordinate of the point to check the neighbors
+            z (int): z coordinate of the point to check the neighbors
+            
+        Return:
+            (np.array): array containing the coordinates of the occupied neighbors
         """
         directions = [
             (1, 0, 0), (-1, 0, 0),  # x-axis
@@ -51,24 +64,65 @@ class Lattice:
                 
         return np.array(neighbors)
 
-    def occupy(self, x: int, y: int, z: int):
-        """_summary_
-
+    def occupy(self, x: int, y: int, z: int) -> None:
+        """
+        Set the status of the cell at coordinates (x,y,z) to occupied.
+        
         Args:
-            x (int): _description_
-            y (int): _description_
-            z (int): _description_
+            x (int): x coordinate of the point to occupy
+            y (int): y coordinate of the point to occupy
+            z (int): z coordinate of the point to occupy
         """
         if self.is_point_inside(x, y, z):
             self.grid[x, y, z] = 1
             
-    def is_occupied(self, x: int, y: int, z: int):
-        """_summary_
+    def is_occupied(self, x: int, y: int, z: int) -> bool:
+        """
+        Function that check if a point at coordinates (x,y,z) i occupied or not.
 
         Args:
             x (int): _description_
             y (int): _description_
             z (int): _description_
+            
+        Return:
+            (bool): 
         """
         return self.grid[x, y, z] == 1
+
+    def set_nucleation_seed(self, x: int, y: int, z: int) -> None:
+        """
+        Function that initialize one nucleation seed.
+        The function does nothing if the selected point is outside the lattice or if it's already a nucleation seed.
+
+        Args:
+            x (int): x coordinate of the nucleation seed
+            y (int): y coordinate of the nucleation seed
+            z (int): z coordinate of the nucleation seed
+        """
+        if self.is_point_inside(x, y, z) and (x, y, z) not in self.initial_seeds:
+            self.occupy(x, y, z)
+            self.initial_seeds.append((x, y, z))
+            
+    def get_active_border(self) -> np.array:
+        """
+        Function that compute the active border of teh crystal.
+        The active border is the set of all empty cells having at least one occupied cell as neighbor.
+
+        Returns:
+            (np.array): array containing the points that form the active border
+        """
+        active_border = []
+        occupied_sites = self.grid[np.where(self.grid == 1)]
+        for (x, y, z) in occupied_sites:
+            neighbors = self.get_neighbors(x, y, z)
+            
+            for (x_n, y_n, z_n) in neighbors:
+                if not self.is_occupied(x_n, y_n, z_n) and (x_n, y_n, z_n) not in active_border:
+                    active_border.append((x_n, y_n, z_n))
+        
+        return np.array(active_border)
+            
+
+
 

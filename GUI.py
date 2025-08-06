174a0d8d54a773fb502979da004d4484dbc97528
Matteo <aguiarimatteo@gmail.com>
2025-08-06 20:02:04 +0200

    Now the GUI is different based on the number of dimentions of the simulation (2 or 3)

--- GUI.py
+++ GUI.py
@@ -45,38 +45,56 @@ def get_visible_voxels_binary_mask(lattice: Lattice) -> np.array:
         
     return binary_mask
 
-def plot_lattice(lattice: Lattice, title: str = "Crystal lattice"):
+def plot_lattice(lattice: Lattice, title: str = "Crystal lattice", three_dim : bool = True):
     """
-    Function that creates an interactive window that plots the 3D lattice.
+    Function that creates an interactive window that plots the 3D (or 2D) lattice.
     The user can inspect it (zoom, rotate, ...) at runtime.
 
     Args:
         lattice (Lattice): lattice to be plotted
         title (str, optional): Title of the canvas. Defaults to "Crystal lattice".
+        three_dim (bool, optional): decides if the crystal is two or three dimentional. Defaults to True.
     """
-    x, y, z = np.nonzero(lattice.grid)
+    if three_dim:
+        x, y, z = np.nonzero(lattice.grid)
     
-    fig = plt.figure(figsize=(8, 7))
-    ax = fig.add_subplot(111, projection='3d')
+        fig = plt.figure(figsize=(8, 7))
+        ax = fig.add_subplot(111, projection='3d')
     
-    voxels = lattice.grid.astype(bool)
-    facecolors = np.where(voxels, 'royalblue', 'none')
-    visible_voxels = get_visible_voxels_binary_mask(lattice)
-    ax.voxels(visible_voxels, facecolors=facecolors, edgecolor='k', linewidth=0.2)
+        voxels = lattice.grid.astype(bool)
+        facecolors = np.where(voxels, 'royalblue', 'none')
+        visible_voxels = get_visible_voxels_binary_mask(lattice)
+        ax.voxels(visible_voxels, facecolors=facecolors, edgecolor='k', linewidth=0.2)
     
-    ax.set_title(title)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+        ax.set_title(title)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
     
-    max_range = max(lattice.shape)
-    ax.set_xlim(0, max_range)
-    ax.set_ylim(0, max_range)
-    ax.set_zlim(0, max_range)
+        max_range = max(lattice.shape)
+        ax.set_xlim(0, max_range)
+        ax.set_ylim(0, max_range)
+        ax.set_zlim(0, max_range)
     
-    ax.view_init(elev=30, azim=45)
-    plt.tight_layout()
-    plt.show()
+        ax.view_init(elev=30, azim=45)
+        plt.tight_layout()
+        plt.show()
+        
+    else:
+        fig, ax = plt.subplots(figsize=(8, 7))
+        ax.set_title(title)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        
+        slice_z = lattice.grid[:, :, lattice.get_nucleation_seeds()[0][2]]
+        occupied = np.argwhere(slice_z)
+        x_list = occupied[:, 0].tolist()
+        y_list = occupied[:, 1].tolist()
+        ax.scatter(x_list, y_list, c='b', s=10)
+        plt.tight_layout()
+        plt.show()
+        
+        
 
 
 if __name__ == '__main__':
@@ -88,5 +106,5 @@ if __name__ == '__main__':
     print(f"\nMean number of steps = {s_mean} +/- {s_std}\nMean restarts = {r_mean} +/- {r_std}\n")
     plot_lattice(LATTICE)"""
     
-    _ = EDEN.EDEN_simulation(LATTICE, 500, False)
-    plot_lattice(LATTICE)
+    _ = EDEN.EDEN_simulation(LATTICE, 1000, False)
+    plot_lattice(LATTICE, three_dim=False)

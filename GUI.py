dfb0883c4df36209f037e4c434fc1fc9f53007be
Matteo <aguiarimatteo@gmail.com>
2025-08-22 16:36:54 +0200

    Added the function compute_voxel_color_on_epoch() in GUI.py

--- GUI.py
+++ GUI.py
@@ -46,6 +46,33 @@ def get_visible_voxels_binary_mask(lattice: Lattice) -> np.array:
         
     return binary_mask
 
+def compute_voxel_color_on_epoch(total_epoch: int, occupation_epoch: int) -> np.array:
+    """
+    This function computes a color depending on when a cell has been occupied.
+    The output color is a linear combination of two colors C1 and C2, defined in the following way:
+        out_color = a * C1 + (1-a) * C2
+        a = occupation_epoch / total_epoch
+
+    Args:
+        total_epoch (int): number of epochs in the simulation.
+        occupation_epoch (int): epoch at which the cell has been occupied.
+
+    Raises:
+        ValueError: if 'total_epoch' or 'occupation_epoch' are negative, the function raises an error.
+        ValueError: if 'total_epoch' is smaller than 'occupation_epoch', the function raises an error.
+
+    Returns:
+        np.array: array representing the color in the form [R, G, B, alpha], each bounded between 0 and 1.
+    """
+    if total_epoch < 0 or occupation_epoch < 0:
+        raise ValueError(f"ERROR: in function 'compute_voxel_color_on_epoch()' the epoch number must be a an integer bigger or equal to zero.")
+    if occupation_epoch > total_epoch:
+        raise ValueError(f"ERROR: in function 'compute_voxel_color_on_epoch()' the occupation epoch must be less than or equal to the total number of epochs.")
+    
+    color = np.array([0, occupation_epoch / total_epoch, (total_epoch - occupation_epoch) / total_epoch, 1])
+    return color
+    
+
 def plot_lattice(lattice: Lattice, title: str = "Crystal lattice", three_dim : bool = True):
     """
     Function that creates an interactive window that plots the 3D (or 2D) lattice.
@@ -102,18 +129,18 @@ def plot_lattice(lattice: Lattice, title: str = "Crystal lattice", three_dim : b
     ax.grid(False)
     plt.tight_layout()
     plt.show()
-        
-        
 
 
-if __name__ == '__main__':
-    LATTICE = Lattice(100, 100, 100)
+if __name__ == '__main__':    
+    # DLA
+    """LATTICE = Lattice(100, 100, 100)
     LATTICE.set_nucleation_seed(0, 50, 50)
     
     s_mean, s_std, r_mean, r_std = DLA.DLA_simulation(LATTICE, 3000, 1, 3, three_dim=True)
     
     print(f"\nMean number of steps = {s_mean} +/- {s_std}\nMean restarts = {r_mean} +/- {r_std}\n")
-    plot_lattice(LATTICE)
+    plot_lattice(LATTICE)"""
     
+    # EDEN
     """_ = EDEN.EDEN_simulation(LATTICE, 1000, False)
     plot_lattice(LATTICE, three_dim=False)"""
